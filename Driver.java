12feb7bb89136ed4df59fc059d1fe6c48bb613dc
Nikhildhuria01 <nikhildhuria01@gmail.com>
2025-10-16 13:15:21 +0530

    Add files via upload

--- Driver.java
+++ Driver.java
@@ -1,13 +1,19 @@
 import java.util.*;
 
-public class Driver{
+ class Driver{
     int did;
     String dname;
     Float drating;
     int tripsCompleted;
     Scanner sc=new Scanner(System.in);
     //takinh 3 input
-    
+    public Driver(int did, String dname, Float drating, int tripsCompleted) {
+        this.did = did;
+        this.dname = dname;
+        this.drating = drating;
+        this.tripsCompleted = tripsCompleted;
+    }
+
     
 }
 class SmartCab{
@@ -32,5 +38,28 @@ class SmartCab{
         for (Driver driver : drivers) {
             System.out.println("Driver ID: " + driver.did + ", Name: " + driver.dname + ", Rating: " + driver.drating + ", Trips Completed: " + driver.tripsCompleted);
         }
+    }
+    public static void main(String[] args) {
+        SmartCab smartCab = new SmartCab();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of drivers to add:");
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            System.out.println("Enter Driver ID, Name, Rating, Trips Completed:");
+            int did = sc.nextInt();
+            String dname = sc.next();
+            Float drating = sc.nextFloat();
+            int tripsCompleted = sc.nextInt();
+            Driver driver = new Driver(did, dname, drating, tripsCompleted);
+            smartCab.addDriver(driver);
+        }
+
+        System.out.println("Drivers sorted by Rating:");
+        smartCab.sortDriversByRating();
+        smartCab.displayDrivers();
+
+        System.out.println("\nDrivers sorted by Trips Completed:");
+        smartCab.sortDriversByTripsCompleted();
+        smartCab.displayDrivers();
     }
 }
